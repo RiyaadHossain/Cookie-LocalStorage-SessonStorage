e04dc83f34b67699a2b8181de06a2181b8445ee7
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-08 21:22:52 +0600

    Location methods

---
+++ location.js
@@ -0,0 +1,4 @@
+location.href //get full address
+location.assign('www.facebook.com') // assign to another website
+location.hash // get the hash of the website
+location.search // get the search of the website
