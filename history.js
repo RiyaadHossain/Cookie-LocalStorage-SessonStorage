b4cdfcf0c21c4b8ca7258443b94c0699ca3913ee
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-08 20:59:40 +0600

    History method added

---
+++ history.js
@@ -0,0 +1,5 @@
+// History Methods
+history.length() //history length
+history.back() // back one step from your current window
+history.forward() // forward one step from your current window
+history.go(n) // go the "n" no. length
